e2d5f53b183f31516f6ba60ca08482026c5710f5
Makhambet Torezhan <makhambet.torezhan@jits-3-26.jits.ipa.dc>
2022-05-04 17:25:15 +0600

    first commit

---
+++ src/main/java/bankapp/Account.java
@@ -0,0 +1,57 @@
+package bankapp;
+
+public class Account {
+    private AccountType accountType;
+    private String id;
+    private String clientId;
+    private double balance;
+    private boolean withdrawAllowed;
+
+    public Account(AccountType accountType, String id, String clientId, double balance, boolean withdrawAllowed) {
+        this.accountType = accountType;
+        this.id = id;
+        this.clientId = clientId;
+        this.balance = balance;
+        this.withdrawAllowed = withdrawAllowed;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean isWithdrawAllowed() {
+        return withdrawAllowed;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setWithdrawAllowed(boolean withdrawAllowed) {
+        this.withdrawAllowed = withdrawAllowed;
+    }
+}
