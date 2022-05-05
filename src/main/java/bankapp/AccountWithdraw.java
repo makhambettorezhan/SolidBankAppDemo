558736d7633c1312034b6e1685ab190afaf03c49
Makhambet Torezhan <makhambet.torezhan@jits-3-26.jits.ipa.dc>
2022-05-05 13:51:54 +0600

    add inhertiance and constructor

--- src/main/java/bankapp/AccountWithdraw.java
+++ src/main/java/bankapp/AccountWithdraw.java
@@ -1,4 +1,7 @@
 package bankapp;
 
-public class AccountWithdraw {
+public class AccountWithdraw extends Account {
+    public AccountWithdraw(AccountType accountType, String id, String clientId, double balance, boolean withdrawAllowed) {
+        super(accountType, id, clientId, balance, withdrawAllowed);
+    }
 }
