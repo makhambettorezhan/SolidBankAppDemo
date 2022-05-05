e58af4770fea9e143045fc077dcfcd4bcf240170
Makhambet Torezhan <makhambet.torezhan@jits-3-26.jits.ipa.dc>
2022-05-05 14:10:45 +0600

    add create account

--- src/main/java/bankapp/AccountCreationServiceImpl.java
+++ src/main/java/bankapp/AccountCreationServiceImpl.java
@@ -9,6 +9,7 @@ public class AccountCreationServiceImpl implements AccountCreationService {
 
     @Override
     public void create(AccountType accountType, long bankId, String clientId, long accountId) {
-
+        Account account = new Account(accountType, accountId+"", clientId, 0, false);
+        accountDAO.createNewAccount(account);
     }
 }
