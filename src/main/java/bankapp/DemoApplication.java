a238b14a9d654716120dc3610a18f92ab2ad81aa
Makhambet Torezhan <makhambet.torezhan@jits-3-26.jits.ipa.dc>
2022-05-05 16:13:32 +0600

    fast commit

--- src/main/java/bankapp/DemoApplication.java
+++ src/main/java/bankapp/DemoApplication.java
@@ -3,11 +3,29 @@ package bankapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		//SpringApplication.run(DemoApplication.class, args);
+		AccountDAO accountDAO = new MemoryAccountDAO();
+		CreateAccountOperationUI createAccountOperationUI = new MyCLI();
+		AccountCreationService accountCreationService = new AccountCreationServiceImpl(accountDAO);
+		AccountListingService accountListingService = new AccountListingServiceImpl(accountDAO);
+
+		BankCore bankCore = new BankCore(accountCreationService);
+		AccountBasicCLI accountBasicCLI = new AccountBasicCLI(createAccountOperationUI, bankCore, accountListingService);
+
+		accountBasicCLI.createAccountRequest("0001");
+		accountBasicCLI.createAccountRequest("0001");
+		accountBasicCLI.createAccountRequest("0001");
+		//accountBasicCLI.createAccountRequest("0002");
+		//accountBasicCLI.createAccountRequest("0003");
+
+		for(Account account: accountBasicCLI.getAccounts("0001")) {
+			System.out.println(account.toString());
+		}
+
 	}
 
 }
