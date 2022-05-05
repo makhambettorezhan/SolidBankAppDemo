a3873b53973ecdb71cba37e2af3ebe108c1c7e1e
Makhambet Torezhan <makhambet.torezhan@jits-3-26.jits.ipa.dc>
2022-05-05 13:22:41 +0600

    add AccountListing: methods

--- src/main/java/bankapp/AccountListingServiceImpl.java
+++ src/main/java/bankapp/AccountListingServiceImpl.java
@@ -7,21 +7,21 @@ public class AccountListingServiceImpl implements AccountListingService {
 
     @Override
     public Account getClientAccount(String clientId, String accountId) {
-        return null;
+        return accountDAO.getClientAccount(clientId, accountId);
     }
 
     @Override
     public AccountWithdraw getClientWithdrawAccount(String clientId, String accountId) {
-        return null;
+        return accountDAO.getClientWithdrawAccount(clientId, accountId);
     }
 
     @Override
     public List<Account> getClientAccounts(String clientId) {
-        return null;
+        return accountDAO.getClientAccounts(clientId);
     }
 
     @Override
     public List<Account> getClientAccountsByType(String clientId, AccountType accountType) {
-        return null;
+        return getClientAccountsByType(clientId, accountType);
     }
 }
