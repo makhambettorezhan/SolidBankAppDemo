5db8a825213cd8c4e51a17ecfbaf497a01e34205
Makhambet Torezhan <makhambet.torezhan@jits-3-26.jits.ipa.dc>
2022-05-05 12:46:50 +0600

    add constructor AccountCreationServiceImpl

--- src/main/java/bankapp/AccountCreationServiceImpl.java
+++ src/main/java/bankapp/AccountCreationServiceImpl.java
@@ -1,6 +1,12 @@
 package bankapp;
 
 public class AccountCreationServiceImpl implements AccountCreationService {
+    private AccountDAO accountDAO;
+
+    public AccountCreationServiceImpl(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
     @Override
     public void create(AccountType accountType, long bankId, String clientId, long accountId) {
 
